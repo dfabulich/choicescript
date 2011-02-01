2f6d499305123255834d069dc5788d1498d5ae36
Dan Fabulich <dan@fabulich.com>
2011-02-01 14:51:05 -0800

    Add choicescript_stats to mygame processing

--- xmltranslator.js
+++ xmltranslator.js
@@ -434,6 +434,8 @@ if (true) {
   load("web/mygame/mygame.js");
   list = [];
   if (typeof(nav) != "undefined") {
+    var statsFile = new java.io.File(dir, nextScene + ".txt");
+    if (statsFile.exists()) list.push(statsFile);
     for (nextScene = nav.getStartupScene(); nextScene; nextScene = nav.nextSceneName(nextScene)) {
       list.push(new java.io.File(dir, nextScene + ".txt"));
     }
