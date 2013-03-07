25dc76ae34a320b1dd801c6bb6c66f8166fef890
CareyJWilliams <careyjwilliams@gmail.com>
2013-03-07 09:34:54 +0000

    Update scene.js

--- web/scene.js
+++ web/scene.js
@@ -2771,5 +2771,5 @@ Scene.validCommands = {"comment":1, "goto":1, "gotoref":1, "label":1, "looplimit
     ,"subscribe":1, "show_password":1, "gosub":1, "return":1, "hide_reuse":1, "disable_reuse":1, "allow_reuse":1
     ,"check_purchase":1,"restore_purchases":1,"purchase":1,"restore_game":1,"advertisement":1
     ,"save_game":1,"delay_break":1,"image":1,"link":1,"input_number":1,"goto_random_scene":1
-    ,"restart":1,"more_games":1,"delay_ending":1
+    ,"restart":1,"more_games":1,"delay_ending":1 "goto_sceneref": 1
     };
