434d574f93d8c90fc89b3906ccc22e598dcae7e8
Dan Fabulich <dan.fabulich@redfin.com>
2013-10-12 16:50:21 -0700

    Allow "*label watch" on Firefox

--- web/scene.js
+++ web/scene.js
@@ -348,7 +348,9 @@ Scene.prototype.parseLabels = function parseLabels() {
         var data = trim(result[3]);
         if ("label" == command) {
             data = data.toLowerCase();
-            if (this.labels[data]) throw new Error(this.lineMsg() + "label '"+data+"' already defined on line " + (this.labels[data]*1+1));
+            if (this.labels.hasOwnProperty(data)) {
+              throw new Error(this.lineMsg() + "label '"+data+"' already defined on line " + (this.labels[data]*1+1));
+            }
             this.labels[data] = this.lineNum;
         }
     }
