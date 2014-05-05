ab22a88dcaedb9292d7d157c1cbb68d599d35c43
Dan Fabulich <dan.fabulich@redfin.com>
2014-05-04 21:13:53 -0700

    comment is the only command allowed in preamble and body

--- web/scene.js
+++ web/scene.js
@@ -421,6 +421,7 @@ Scene.prototype.runCommand = function runCommand(line) {
     var command = result[1].toLowerCase();
     var data = trim(result[2]);
     if (Scene.validCommands[command]) {
+        if ("comment" == command) return true;
         if (Scene.initialCommands[command]) {
           if ("startup" != this.name || !this.screenEmpty || !this.initialCommands) {
             throw new Error(this.lineMsg() + "Invalid "+command+" instruction, only allowed at the top of startup.txt");
