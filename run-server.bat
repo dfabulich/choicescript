d1f1f5746021f8ba8b67bc87a4136c0777e8373f
Dan Fabulich <dan@fabulich.com>
2019-07-23 21:53:01 -0700

    Add convenience scripts for serving ChoiceScript via a web server

---
+++ run-server.bat
@@ -0,0 +1,20 @@
+
+::Copyright 2010 by Dan Fabulich.
+::
+::Dan Fabulich licenses this file to you under the
+::ChoiceScript License, Version 1.0 (the "License"); you may
+::not use this file except in compliance with the License. 
+::You may obtain a copy of the License at
+::
+:: http://www.choiceofgames.com/LICENSE-1.0.txt
+::
+::See the License for the specific language governing
+::permissions and limitations under the License.
+::
+::Unless required by applicable law or agreed to in writing,
+::software distributed under the License is distributed on an
+::"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+::either express or implied.
+@echo off
+node serve
+pause
