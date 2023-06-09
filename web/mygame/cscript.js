31a774b442ae5570b50a5723c09d0c0966aac4f5
Andrew Long <AndrewRLong02@gmail.com>
2023-06-08 19:44:00 -0700

    Refactor dictionary code to stop the edge cases I don't wanna deal with

--- web/mygame/cscript.js
+++ web/mygame/cscript.js
@@ -10,6 +10,9 @@ function storeOriginalReplaceBbCode(func) {
 /* This is where we add our own formatting tags. */
 function replacementReplaceBbCode(msg) {
     return msg = String(originalReplaceBbCode(msg))
+      // Start and end tags for color 
       .replace(/\[color\=(.*?)\]/g, '<color style="color: $1;">')
-      .replace(/\[\/color\]/g, '</color>')        
+      .replace(/\[\/color\]/g, '</color>')
+      // Tag for the dictionary system
+      .replace(/\[define\:(.*?)\]/g, "<a id=\"defined-word\" onClick=\"openDefinition('$1')\">$1</a>")
 }
--- web/mygame/dict.js
+++ web/mygame/dict.js
@@ -3,58 +3,51 @@ const dict = new Map();
 //-------------------------- Dictionary Definitions --------------------------\\
 dict.set('the', makeDefinition("The", "/ðə/, /ˈðiː/", "Article", "Definite grammatical article that implies necessarily that an entity it hints at is presupposed; something already mentioned, or completely specified later in that same sentence, or assumed already completely specified."));
 dict.set('see', makeDefinition("See", "/ˈsiː/", "Verb", "To perceive or detect someone or something with the eyes, or as if by sight."));
+dict.set('uat', makeDefinition("UAT", null, null, "To perceive or detect someone or something with the eyes, or as if by sight."));
 //----------------------------------------------------------------------------\\
 
 function makeDefinition(word, pronounce, pos, def) {
-    return { word: word, pronunciation: pronounce, partOfSpeech: pos, definition: def };
+    return {
+        word: word,
+        pronunciation: pronounce,
+        partOfSpeech: pos,
+        definition: def
+    };
 }
 
 function openDefinition(inp) {
     if (document.getElementById('loading')) return;
 
-    let tmp = dict.get(inp);
-    let temp = new Scene("dict", null, this.nav, { secondaryMode: "startup" });
+    // Get definition from dict hashmap
+    let tmp = dict.get(inp.toLowerCase());
+    let temp = new Scene("dict", null, this.nav, { secondaryMode: "dict" });
 
+    // Null check for easy readin'
+    let nullCheck = (input, tester) => {
+        return (tester == null ? '' : input)
+    }
+
+    // Create 
     clearScreen(function () {
         setButtonTitles();
-        // REMINDER: Remember to delete the temp text
-        document.getElementById("text").innerHTML = `
-            <replaceignore></replaceignore>
-            <span id="temp-text">This is a quick test of the dictionary feature. There will be bugs. - Long</span>
-            <div id="dict-top-row">
-            <span id="dict-word">${tmp.word}</span>
-            <span>•</span>
-            <span id="dict-pos">${tmp.partOfSpeech}</span>
-            <span>•</span>
-            <span id="dict-pronounce">${tmp.pronunciation}</span>
-            </div>
-            <p id="dict-definition">${tmp.definition}</p>`;
+        document.getElementById("text").innerHTML =`
+            <div id="dict-top-row">` + 
+            nullCheck(`<span id="dict-word">${tmp.word}</span>`, tmp.word) + 
+            nullCheck(`<span>•</span><span id="dict-pos">${tmp.partOfSpeech}</span>`, tmp.partOfSpeech) +
+            nullCheck(`<span>•</span><span id="dict-pronounce">${tmp.pronunciation}</span>`, tmp.pronunciation) + `
+            </div>` +
+            nullCheck(`<p id="dict-definition">${tmp.definition}</p>`, tmp.definition);
 
         printButton("Back", main, false, () => {
             clearScreen(loadAndRestoreGame);
         })
 
         temp.execute();
-        wordReplacer();
     });
 }
 
-function findWordAndReplaceWithDefinition(inp) {
-    var main = document.getElementById("text")
-    if (main.getElementsByTagName("replaceignore").length != 0) return;
 
-    var tml = main.innerHTML.split(" ");
-
-    tml.forEach((v, i, a) => {
-        tml[i] = v.replace(new RegExp(
-            `\\b${inp}(?![a-z,A-Z,0-9].*)`, "i"),
-            `<a id="defined-word" onclick="openDefinition('${inp}')">$&</a>`
-        );
-    })
-
-    main.innerHTML = tml.join(" ");
-}
-
+// Maybe this will be useful later...
 // modified code from https://stackoverflow.com/a/61511955
 function doAfterLoad(func) {
     let selector = "#loading"
@@ -75,23 +68,4 @@ function doAfterLoad(func) {
             subtree: true
         });
     }).then(func);
-}
-
-function wordReplacer() {
-    doAfterLoad(() => {
-        // if(document.getElementsByTagName("processed").length != 0) return;
-
-        dict.forEach((v, k) => {
-            findWordAndReplaceWithDefinition(k);
-        })
-
-        Array.from(document.getElementsByTagName("button")).forEach((v, i, a) => {
-            v.addEventListener("click", () => {
-                wordReplacer();
-            });
-        });
-
-        document.getElementById("text").append(document.createElement("processed"));
-
-    })
 }
